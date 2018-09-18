b72dc6b10b094331abb398ba1631863193b5172a
Evgeny Uleykin <android73.dd@gmail.com>
2018-09-19 01:15:41 +0300

    Calculate new value through RxJava2

--- app/src/main/java/ru/android73dd/androidpopularlibs/CounterModel.java
+++ app/src/main/java/ru/android73dd/androidpopularlibs/CounterModel.java
@@ -3,9 +3,13 @@ package ru.android73dd.androidpopularlibs;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.subjects.PublishSubject;
+
+
 public class CounterModel
 {
     List<Integer> counters;
+    List<PublishSubject<Integer>> subjects;
 
     public CounterModel()
     {
@@ -13,11 +17,27 @@ public class CounterModel
         counters.add(0);
         counters.add(0);
         counters.add(0);
+        subjects = new ArrayList<>();
+        PublishSubject<Integer> subject1 = PublishSubject.create();
+        PublishSubject<Integer> subject2 = PublishSubject.create();
+        PublishSubject<Integer> subject3 = PublishSubject.create();
+        subjects.add(subject1);
+        subjects.add(subject2);
+        subjects.add(subject3);
     }
 
-    public Integer calculate(int index)
+    public void calculate(int index)
     {
-        counters.set(index, counters.get(index) + 1);
-        return counters.get(index);
+        int value = counters.get(index) + 1;
+        counters.set(index, value);
+        subjects.get(index).onNext(value);
+    }
+
+    public PublishSubject<Integer> getSubject(int index) {
+        return subjects.get(index);
+    }
+
+    public List<PublishSubject<Integer>> getSubjects() {
+        return subjects;
     }
 }
--- app/src/main/java/ru/android73dd/androidpopularlibs/MainPresenter.java
+++ app/src/main/java/ru/android73dd/androidpopularlibs/MainPresenter.java
@@ -3,25 +3,118 @@ package ru.android73dd.androidpopularlibs;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.PublishSubject;
+
 @InjectViewState
 public class MainPresenter extends MvpPresenter<MainView>
 {
-    CounterModel model;
+    private CounterModel model;
+    private Observer obServerValue1;
+    private Observer obServerValue2;
+    private Observer obServerValue3;
+    private Disposable disposable1;
+    private Disposable disposable2;
+    private Disposable disposable3;
+    private List<PublishSubject<Integer>> subjects;
 
     public MainPresenter()
     {
         this.model = new CounterModel();
+        subjects = model.getSubjects();
+        obServerValue1 = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable1 = d;
+            }
+
+            @Override
+            public void onNext(Object o) {
+                getViewState().updateButtonOneText(String.valueOf(o));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        obServerValue2 = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable2 = d;
+            }
+
+            @Override
+            public void onNext(Object o) {
+                getViewState().updateButtonTwoText(String.valueOf(o));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        obServerValue3 = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable3 = d;
+            }
+
+            @Override
+            public void onNext(Object o) {
+                getViewState().updateButtonThreeText(String.valueOf(o));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    @Override
+    public void attachView(MainView view) {
+        super.attachView(view);
+        model.getSubject(0).subscribe(obServerValue1);
+        model.getSubject(1).subscribe(obServerValue2);
+        model.getSubject(2).subscribe(obServerValue3);
+    }
+
+    @Override
+    public void detachView(MainView view) {
+        disposable1.dispose();
+        disposable2.dispose();
+        disposable3.dispose();
+        super.detachView(view);
     }
 
     public void onButtonOneClick() {
-        getViewState().updateButtonOneText(String.valueOf(model.calculate(0)));
+        model.calculate(0);
     }
 
     public void onButtonTwoClick() {
-        getViewState().updateButtonTwoText(String.valueOf(model.calculate(1)));
+        model.calculate(1);
     }
 
     public void onButtonThreeClick() {
-        getViewState().updateButtonThreeText(String.valueOf(model.calculate(2)));
+        model.calculate(2);
     }
 }
